bb859077cfe5a1c8d5ff3b88f83f171563bad1b7
HaraldurJoh <zeasick@gmail.com>
2021-02-09 14:54:27 +0100

    Login på elev og lærer

--- src/sample/Controllers/LoginViewController.java
+++ src/sample/Controllers/LoginViewController.java
@@ -9,8 +9,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.*;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,29 +17,34 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginViewController implements Initializable {
-    public Button logIn;
+    @FXML
+    private TextField UserName;
 
     @FXML
+    private TextField PassWord;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
     }
 
     @FXML
-    public void login(ActionEvent event) {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("../GUI/StudentView.fxml"));
+    public void login(ActionEvent event) throws Exception {
+        if (UserName.getText().equals("elev") && PassWord.getText().equals("elev")) {
             Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("../GUI/StudentView.fxml"));
             Scene scene = new Scene(root);
             stage.setScene(scene);
-            stage.showAndWait();
-        } catch (IOException e) {
-            e.printStackTrace();
+            stage.show();
+        }
+        else if (UserName.getText().equals("lærer") && PassWord.getText().equals("lærer"));
+        Parent root = FXMLLoader.load(getClass().getResource("../GUI/TeacherView.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
     }
-}
+    
     public void handleSelectStudent(ActionEvent actionEvent) {
-        
     }
-
 
 }
