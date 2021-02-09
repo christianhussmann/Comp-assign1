e43a000f9365f36d1fe54365dd944d5357327e79
Mathias Kristensen <mathiaskristensen52@gmail.com>
2021-02-09 13:10:06 +0100

    Student class

--- src/sample/BE/Student.java
+++ src/sample/BE/Student.java
@@ -1,4 +1,74 @@
 package sample.BE;
 
+import java.util.ArrayList;
+
 public class Student {
+
+
+    private int semester;
+    private String name;
+    private String classYear;
+    private String education;
+    private ArrayList<String> subjects;
+    private int attendance;
+
+    public Student(int semester, String name, String classYear, String education, int attendance) {
+        setAttendance(attendance);
+        setSemester(semester);
+        setClassYear(classYear);
+        setName(name);
+        setEducation(education);
+    }
+    
+    public int getAttendance() {
+        return attendance;
+    }
+
+    public void setAttendance(int attendance) {
+        this.attendance = attendance;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClassYear() {
+        return classYear;
+    }
+
+    public void setClassYear(String classYear) {
+        this.classYear = classYear;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public ArrayList<String> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(ArrayList<String> subjects) {
+        this.subjects = subjects;
+    }
+
+
+
+
 }
