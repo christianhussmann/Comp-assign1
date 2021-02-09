a9cac0176c294fd1e6a5c16e2133d43b303b11c5
TrineKnudsen <trinesaknudsen@hotmail.com>
2021-02-09 09:18:58 +0100

    ElevView

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("LæreView.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1500, 975));
         primaryStage.show();
