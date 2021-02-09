d287f8dc700614ed37cc7c0b15cfd452a464fa15
Mathias Kristensen <mathiaskristensen52@gmail.com>
2021-02-09 13:25:38 +0100

    csdsd

--- src/sample/DAL/StudentDAL.java
+++ src/sample/DAL/StudentDAL.java
@@ -13,10 +13,15 @@ public class StudentDAL {
         ObservableList<Student> allStudents = FXCollections.observableArrayList();
 
         Student s1 = new Student(2, "Mathias", "Kristensen", "CSe_20A", "Datamatiker", 20);
+        allStudents.add(s1);
         Student s2 = new Student(2, "Haraldur", "Jóhannesson", "CSe_20A", "Datamatiker", 20);
+        allStudents.add(s2);
         Student s3 = new Student(2, "Trine", "Knudsen", "CSe_20A", "Datamatiker", 20);
+        allStudents.add(s3);
         Student s4 = new Student(2, "Julian", "Petersen", "CSe_20A", "Datamatiker", 20);
+        allStudents.add(s4);
         Student s5 = new Student(2, "Christian", "Hussmann", "CSe_20A", "Datamatiker", 20);
+        allStudents.add(s5);
         return allStudents;
     }
 }
